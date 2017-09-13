98ab9ca7d5c4bc53220319df538469c6662c2b4c
eduardosm7 <esm7@cin.ufpe.br>
2017-09-13 16:59:25 -0300

    Create map with words and scores

--- main.cpp
+++ main.cpp
@@ -37,9 +37,9 @@ int main ()
         transform(train_set_raw[i].begin(), train_set_raw[i].end(), train_set_raw[i].begin(), ::tolower);
     }
 
-    //Splits the phrases and add each word to a map
+    //Splits the phrases and add each word to a map, with it's scores and couters
 
-    map<string,pair<int,int>> words; //<word,total_score,counter>
+    map<string,pair<int,int>> words; //<word,pair<total_score,counter>>
 
     for(int i = 0; i < (train_set_raw.size() - 1); i++)
     {
@@ -52,7 +52,7 @@ int main ()
         for(int j = 1; j < (train_set_clean.size() - 1); j++)
         {
 
-            if ((words.find(train_set_clean[j]) == words.end()) && (stop_words.find(train_set_clean[j]) == stop_words.end()))
+            if ((words.find(train_set_clean[j]) == words.end()) && (stop_words.find(train_set_clean[j]) == stop_words.end())) //Checks if word is already in the map and if it's a stop word
             {
                 words[train_set_clean[j]] = pair<int,int>(score,1);
 
@@ -63,6 +63,16 @@ int main ()
                 words[train_set_clean[j]].second++;
             }
         }
+    }
+
+    //Creates a map with the pair <word,score>
+
+    map<string,int> words_scores;
+    map<string,pair<int,int>>::iterator it;
+
+    for(it = words.begin(); it != words.end(); it++)
+    {
+        cout << it->second.second << endl;
     }
 
 }
